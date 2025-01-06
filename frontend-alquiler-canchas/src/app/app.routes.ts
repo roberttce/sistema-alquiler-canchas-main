9b2c2d3467c8fe71fe1b97bcab3e7e4ef2dbf8ba
SamuelF32 <raulmontesinosvaldivia@gmail.com>
2025-01-06 01:17:02 -0500

    [add] implementacion vista reserva clientes de la pagina web statica

--- frontend-alquiler-canchas/src/app/app.routes.ts
+++ frontend-alquiler-canchas/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/Landing/login/login.component';
 import { InicioComponent } from './components/Landing/inicio/inicio.component';
+import { HomeComponent } from './components/PaginaWeb/home/home.component';
+import { ReservausuarioComponent } from './components/PaginaWeb/reservausuario/reservausuario.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
-    { path: 'inicio', component: InicioComponent }
+    { path: 'inicio', component: InicioComponent },
+    { path: 'home', component: HomeComponent }, //ruta para HomeComponent
+    { path: 'reservausuario', component: ReservausuarioComponent }
 ];
