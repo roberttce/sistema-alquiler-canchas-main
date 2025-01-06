4261a24630db8f598c3685b90768c418be716257
vrfvrgbt <221162@unamba.edu.pe>
2025-01-06 01:53:36 -0500

    inicio sesion

--- frontend-alquiler-canchas/src/app/app.routes.ts
+++ frontend-alquiler-canchas/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/Landing/login/login.component';
 import { InicioComponent } from './components/Landing/inicio/inicio.component';
+import { GetallComponent } from './components/Landing/clientes/getall/getall.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
-    { path: 'inicio', component: InicioComponent }
+    { path: 'inicio', component: InicioComponent },
+    { path: 'Clientes/getall', component: GetallComponent}
+    
 ];
